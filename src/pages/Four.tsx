21b04fdf2bc8ab764a6917766ba6a20abf86a185
JehyunNoteBook <povt99@gmail.com>
2023-01-03 13:22:18 +0900

    commit

--- src/pages/Four.tsx
+++ src/pages/Four.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useRef, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {
   View,
   Text,
@@ -12,41 +12,30 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {useSelector} from 'react-redux';
 import {RootState} from '../store/reducer';
 import axios, {AxiosError} from 'axios';
+import exampleSlice from '../slices/example';
+import {useAppDispatch} from '../store';
 // 이메일, 비밀번호를 둘다 입력해야 버튼 활성화
 // 스택 라우터
 type Fourtype = NativeStackScreenProps<FourFive, 'Four'>;
 function Four({navigation}: Fourtype) {
-  // 로딩
+  //로딩 창
   const [isLaoding, setIsLoading] = useState(false);
+  //dispatch 가져오기
+  const dispatch = useAppDispatch();
   // state 가져오기
   const email = useSelector((state: RootState) => state.exam.email);
-  const name = useSelector((state: RootState) => state.exam.name);
-  // 서버에 데이터 전송
-  const onSubmit = useCallback(async () => {
-    try {
-      // 비동기 작업하기 전에 로딩을 true로 변경
-      setIsLoading(true);
-      // 비동기니까 await
-      const response = await axios.post(
-        // 첫번째 : 통신 주소
-        '/user',
-        // 두번째 : 보낼 데이터
-        {email, name},
-        // 세번째 : 헤더 주로 토큰을 보냄
-        {headers: {authorization: `Bearer`}},
-      );
-    } catch (error) {
-      //axios 에러 타입을 넣어줘야 함
-      const errorResponse = (error as AxiosError).response;
-      // 에러가 발생하면 Alert 창 띄우기
-      if (errorResponse) {
-        Alert.alert('알람', (errorResponse.data as any).message);
-      }
-    } finally {
-      //로딩 없애기
-      setIsLoading(false);
+  useEffect(() => {
+    async function getMoney() {
+      //get으로 받아오는 타입은 알수 없기 때문에 get옆에 무슨 타입이 오는지 명시 해줘야 한다.
+      //axios.get<{data: number}> get으로 받아오는 타입은
+      //response.data.data 숫자 타입 :: data 숫자 타입
+      const response = await axios.get<{data: number}>(`/showmethemoney`, {
+        headers: {authorization: `Bearer ${email}`},
+      });
+      dispatch(exampleSlice.actions.setUser(response.data.data));
     }
-  }, [isLaoding, email, name]);
+    getMoney();
+  }, [dispatch]);
   return (
     <>
       <View>
@@ -56,7 +45,6 @@ function Four({navigation}: Fourtype) {
         ) : (
           <Pressable
             style={{backgroundColor: 'blue'}}
-            onPress={onSubmit}
             //axios 통신 중일 때 버튼을 여러번 눌러 요청 할 수 없도록 disabled를 적용한다.
             disabled={isLaoding}>
             <Text>버튼</Text>
