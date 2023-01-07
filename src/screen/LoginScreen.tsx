a9aa6eb8c00facf1b84f36511ce1f40b1af5bb04
BHyeonKim <rlaqhguse@gmail.com>
2023-01-07 15:50:54 +0900

    디렉토리 설정

---
+++ src/screen/LoginScreen.tsx
@@ -0,0 +1,7 @@
+import {Text} from 'react-native';
+
+const LoginScreen = () => {
+  return <Text>여기</Text>;
+};
+
+export default LoginScreen;
---
+++ src/screen/SignupScreen.tsx
@@ -0,0 +1,7 @@
+import {Text} from 'react-native';
+
+const SignupScreen = () => {
+  return <Text>여기</Text>;
+};
+
+export default SignupScreen;
