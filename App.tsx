f5415491b315580003466efd7d0f6057cb92fe03
BHyeonKim <rlaqhguse@gmail.com>
2023-01-07 15:04:25 +0900

    린터 설정 및 설정

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
-import {View,Text} from "react-native";
-
+import {View, Text} from 'react-native';
 
 function App() {
   return (
-<View>
-<Text>
-asdsad
-</Text>
-</View>
+    <View>
+      <Text>asdsad</Text>
+    </View>
   );
 }
 
